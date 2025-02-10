e191165e4ded4952614c2da81c9a7165d881a990
Minh An Dao <minhan.dao@student.unimelb.edu.au>
2025-02-10 23:52:06 +1100

    added visualised directly ML estimate for x and y

--- go.m
+++ go.m
@@ -1,6 +1,6 @@
 % add folders to search path
 cur_dir = pwd;
-folders = {'doa'};
+folders = {'doa','localisation'};
 for ii = 1:length(folders)
     addpath(genpath(fullfile(cur_dir, folders{ii})));
 end
